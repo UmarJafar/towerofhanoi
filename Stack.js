ec381ac15c275e90e008da430312727bad4b4c35
Umar Jafar <umardev.seedout@gmail.com>
2023-06-13 11:10:32 +0500

    initial commit

--- Stack.js
+++ Stack.js
@@ -1,36 +1,35 @@
-class Stack{
-	constructor(){
-		this.A = [];
-		this.tos = 0;
-	}
-
-	isEmpty(){
-		return !this.tos;
-	}
-	empty(){
-		for(let i=0; i<this.tos; i++){
-			delete this.A[i]; 
-		}
-		this.tos = 0;
-	}
-
-	push(item){
-		this.A[this.tos++] = item; 
-	}
-	pop(){
-		return this.A[--this.tos];
-	}
-	peek(){
-		return this.A[this.tos-1];
-	}
-	length(){
-		return this.tos;
-	}
-	print(){
-		var str = "";
-		for(let i=this.tos-1; i>=0; i--){
-			str += this.A[i] + "\n"; 
-		}
-		return str;
-	}
-}
+var Stack = /** @class */ (function () {
+    function Stack() {
+        this.A = [];
+        this.tos = 0;
+    }
+    Stack.prototype.isEmpty = function () {
+        return !this.tos;
+    };
+    Stack.prototype.empty = function () {
+        for (var i = 0; i < this.tos; i++) {
+            delete this.A[i];
+        }
+        this.tos = 0;
+    };
+    Stack.prototype.push = function (item) {
+        this.A[this.tos++] = item;
+    };
+    Stack.prototype.pop = function () {
+        return this.A[--this.tos];
+    };
+    Stack.prototype.peek = function () {
+        return this.A[this.tos - 1];
+    };
+    Stack.prototype.length = function () {
+        return this.tos;
+    };
+    Stack.prototype.print = function () {
+        var str = "";
+        for (var i = this.tos - 1; i >= 0; i--) {
+            str += this.A[i] + "\n";
+        }
+        return str;
+    };
+    return Stack;
+}());
